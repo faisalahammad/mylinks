9807edc66b27e073ff4f27bef1c118e0b0b4a096
bacloud14 <bacloud14@gmail.com>
2021-03-26 14:28:47 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// var form = document.querySelector("form");
+// form.addEventListener("submit", function (event) {
+//     console.log("Saving value", form.elements.value.value);
+//     event.preventDefault();
+// });
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const formJSON = Object.fromEntries(data.entries());
+    // for multi-selects, we need special handling
+    formJSON.snacks = data.getAll('snacks');
+    // const results = document.querySelector('.results pre');
+    console.log(JSON.stringify(formJSON, null, 2));
+}
+
+const form = document.querySelector('.contact-form');
+form.addEventListener('submit', handleFormSubmit);
